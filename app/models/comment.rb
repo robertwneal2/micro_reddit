b6f44c71bc031ba0470fac74382dfd1aa531f652
Robert Neal <robertwneal2@gmail.com>
2022-10-13 17:56:54 -0700

    Remove belongs_to validations since those are automatic in Rails 5.0+

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,5 @@
 class Comment < ApplicationRecord
-  validates :body, :user, :post, presence: true
+  validates :body, presence: true
 
   belongs_to :user
   belongs_to :post
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,5 @@
 class Post < ApplicationRecord
-  validates :title, :url, :user, presence: true
+  validates :title, :url, presence: true
   validates :url, uniqueness: true
 
   belongs_to :user
